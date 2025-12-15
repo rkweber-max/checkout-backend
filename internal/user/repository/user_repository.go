7724b28974a7d41c089247fd0360a5dce5df379c
Rodrigo Kunzler Weber <rodrigo.weber@appmax.com.br>
2025-12-15 10:10:25 -0300

    feat: Build User Repository

---
+++ internal/user/repository/user_repository.go
@@ -0,0 +1,67 @@
+package repository
+
+import (
+	"errors"
+
+	"github.com/rkweber-max/checkout-backend/internal/user/domain"
+	"gorm.io/gorm"
+)
+
+type UserRepository interface {
+	Create(user *domain.User) error
+	FindByID(id uint) (*domain.User, error)
+	FindByEmail(email string) (*domain.User, error)
+	List() ([]domain.User, error)
+	Update(user *domain.User) error
+	Delete(id uint) error
+}
+
+type userRepository struct {
+	db *gorm.DB
+}
+
+func NewUserRepository(db *gorm.DB) UserRepository {
+	return &userRepository{db: db}
+}
+
+func (r *userRepository) Create(user *domain.User) error {
+	return r.db.Create(user).Error
+}
+
+func (r *userRepository) FindByID(id uint) (*domain.User, error) {
+	var user domain.User
+
+	err := r.db.First(&user, id).Error
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		return nil, nil
+	}
+
+	return &user, err
+}
+
+func (r *userRepository) FindByEmail(email string) (*domain.User, error) {
+	var user domain.User
+
+	err := r.db.Where("email = ?", email).First(&user).Error
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		return nil, nil
+	}
+
+	return &user, err
+}
+
+func (r *userRepository) List() ([]domain.User, error) {
+	var users []domain.User
+
+	err := r.db.Find(&users).Error
+
+	return users, err
+}
+
+func (r *userRepository) Update(user *domain.User) error {
+	return r.db.Save(user).Error
+}
+
+func (r *userRepository) Delete(id uint) error {
+	return r.db.Delete(&domain.User{}, id).Error
+}
